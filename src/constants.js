9a9193e21ce2425a4bfbf1212059a7c486f21a02
Александра <alexandrasmirnova230903@gmail.com>
2025-09-30 21:47:54 +0300

    Lesson 4: to React.js (first page)

---
+++ src/constants.js
@@ -0,0 +1,50 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Пвх материал 2й сорт (1)',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 1,
+        title: 'Пвх материал 2й сорт (2)',
+        price: '171 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 2,
+        title: 'Пвх материал 2й сорт (3)',
+        price: '172 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 3,
+        title: 'Пвх материал 2й сорт (4)',
+        price: '173 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 4,
+        title: 'Пвх материал 2й сорт (5)',
+        price: '174 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    },
+    {
+        id: 5,
+        title: 'Пвх материал 2й сорт (6)',
+        price: '175 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png'
+    }
+]
